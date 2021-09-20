ee10ef8d3cd53c6fb1308b9cbec42c7479629651
AudrainCHANEL <audrain.chanel21000@gmail.com>
2021-09-20 15:25:09 +0200

    Premier git

---
+++ app/src/main/java/fr/audrain/appcoeur/JeSuis.java
@@ -0,0 +1,14 @@
+package fr.audrain.appcoeur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JeSuis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_je_suis);
+    }
+}
